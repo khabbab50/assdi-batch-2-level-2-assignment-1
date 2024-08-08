4c5571758a2ca2b111296f59c4fc40fbf503de63
Md Khabbab Hossen <sk.khabbab50@gmail.com>
2024-08-08 17:37:36 +0600

    mobile menu fixing

--- dist/assets/script.js
+++
@@ -1,6 +0,0 @@
-const mobilIcon = document.querySelector("#mobilIcon");
-const mainMenu = document.querySelector("#mainMenu");
-
-mobilIcon.addEventListener("click", () => {
-  mainMenu.classList.toggle("invisible");
-});
