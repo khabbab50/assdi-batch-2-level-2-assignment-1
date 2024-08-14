5514e5e0958109f82aea4ab80ec69617093b7873
Md Khabbab Hossen <sk.khabbab50@gmail.com>
2024-08-14 11:08:41 +0600

    singIn page not fixing

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const mobilIcon = document.querySelector("#mobilIcon");
 const mainMenu = document.querySelector("#mainMenu");
+const singInPage = document.querySelector("#singInPage");
 
 mobilIcon.addEventListener("click", () => {
   mainMenu.classList.toggle("invisible");
+});
+
+singInPage.addEventListener("click", function () {
+  window.location = "../singIn.html";
 });
